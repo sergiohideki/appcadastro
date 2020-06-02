d4c4b53375a4cabb5462adb7172b8fbdcb783346
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-06-02 14:12:53 -0300

    interface de cadastro e puxando dados hard coded

---
+++ appcadastro/lib/data/dummy_users.dart
@@ -0,0 +1,34 @@
+import 'package:appcadastro/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Homer Simpsons',
+    email: 'homer@gmail.com',
+    avatarUrl: 'https://i.pinimg.com/originals/f3/b6/b2/f3b6b24cfe8deff4608649fdc6b96b34.jpg'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Marge Simpsons',
+    email: 'marge@gmail.com',
+    avatarUrl: 'https://www.pinclipart.com/picdir/middle/54-548650_the-simpsons-clipart-marge-simpson-marge-simpson-webbed.png'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Lisa Simpsons',
+    email: 'lisa@gmail.com',
+    avatarUrl: 'https://vignette.wikia.nocookie.net/simpsons/images/d/d5/Lisa_Simpson_official.png/revision/latest/top-crop/width/360/height/360?cb=20190409004811'
+  ),
+  '4': const User(
+    id: '4',
+    name: 'bart Simpsons',
+    email: 'bart@gmail.com',
+    avatarUrl: 'https://img.favpng.com/6/15/2/bart-simpson-marge-simpson-homer-simpson-image-the-simpsons-png-favpng-yu6kB2mZ59dtuhKuuNqLfjbhD.jpg'
+  ),
+  '5': const User(
+    id: '5',
+    name: 'Maggie Simpsons',
+    email: 'maggie@gmail.com',
+    avatarUrl: 'https://i.pinimg.com/474x/46/5a/b3/465ab307f622420643e01b578a810bcb.jpg'
+  ),
+};
--- appcadastro/lib/views/user_list.dart
+++ appcadastro/lib/views/user_list.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:appcadastro/data/dummy_users.dart';
+import 'package:appcadastro/components/user_tile.dart';
+
 class UserList extends StatelessWidget {
+  final users = DUMMY_USERS;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista de usuários'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Lista de usuários'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+        ));
   }
 }
